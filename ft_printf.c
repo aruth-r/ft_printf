1fb89df6283df83fef1cc82a35c8b269257ef377
aruth-r <81265785+aruth-r@users.noreply.github.com>
2021-08-06 23:33:19 -0300

    Add files via upload

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: aruth-ra <aruth-ra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/24 19:09:00 by aruth-ra          #+#    #+#             */
-/*   Updated: 2021/08/04 23:22:41 by aruth-ra         ###   ########.fr       */
+/*   Updated: 2021/08/06 23:31:22 by aruth-ra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,27 +18,25 @@ int	ft_putchar_fd(char c, int fd)
 	return (1);
 }
 
-int	ft_print_type(const char *str, va_list args, int *i, int *k)
+int	ft_print_type(const char *str, va_list args, int *i)
 {
-	*k = *k + 1;
 	if (str[*i] == 'c')
-		return (ft_printchar(args, *k));
+		return (ft_printchar(args));
 	else if (str[*i] == 's')
-		return (ft_printstring(args, *k));
+		return (ft_printstring(args));
 	else if (str[*i] == 'd' || str[*i] == 'i')
-		return (ft_printnum(args, *k));
+		return (ft_printnum(args));
 	else if (str[*i] == 'u')
-		return (ft_printuns(args, *k));
+		return (ft_printuns(args));
 	else if (str[*i] == 'p')
-		return (ft_printhex(args, str[*i], *k) + 2);
+		return (ft_printhex(args, str[*i]) + 2);
 	else if (str[*i] == 'x' || str[*i] == 'X')
-		return (ft_printhex(args, str[*i], *k));
+		return (ft_printhex(args, str[*i]));
 	else
 	{
 		write(1, "%", 1);
 		if (str[*i] != '%')
 			ft_putchar_fd(str[*i], 1);
-		*k = *k - 1;
 	}
 	return (1);
 }
@@ -48,18 +46,16 @@ int	ft_printf(const char *str, ...)
 	va_list	args;
 	int		i;
 	int		j;
-	int 	k;
 
 	i = 0;
 	j = 0;
-	k = 0;
 	va_start(args, str);
 	while (str[i] != '\0')
 	{
 		if (str[i] == '%')
 			{
 				i++;
-				j += ft_print_type(str, args, &i, &k);
+				j += ft_print_type(str, args, &i);
 			}
 		else
 			j += ft_putchar_fd(str[i], 1);
